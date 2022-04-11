91c2b8a853a84fba892f6ed3fb68ac115ac656f8
ngocphuphamm <ngocphupham682001@gmail.com>
2022-04-11 16:25:01 +0700

    search

---
+++ ClothesWebNET/Controllers/SearchController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClothesWebNET.Models;
+
+namespace ClothesWebNET.Controllers
+{
+    public class SearchController : Controller
+    {
+        private CLOTHESEntities db = new CLOTHESEntities();
+
+        // GET:  http://localhost:46418/search/index?=tin
+        public ActionResult Index(string q)
+        {
+           // queryparamater = "glasses";
+            var product = from el in db.Products
+                          select el;
+         
+            if (!String.IsNullOrEmpty(q))
+            {
+                q = q.ToLower();
+                product = product.Where(s => s.nameProduct.ToLower().Contains(q));
+               
+            }
+            return View(product.ToList());
+        }
+
+        // GET: Search/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Search/Create
+        public ActionResult Create()
+        {
+            ViewBag.idType = new SelectList(db.Types, "idType", "nameType");
+            return View();
+        }
+
+        // POST: Search/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nameProduct,idProduct,sizeM,sizeL,sizeXL,price,description,idType")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idType = new SelectList(db.Types, "idType", "nameType", product.idType);
+            return View(product);
+        }
+
+        // GET: Search/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idType = new SelectList(db.Types, "idType", "nameType", product.idType);
+            return View(product);
+        }
+
+        // POST: Search/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "nameProduct,idProduct,sizeM,sizeL,sizeXL,price,description,idType")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idType = new SelectList(db.Types, "idType", "nameType", product.idType);
+            return View(product);
+        }
+
+        // GET: Search/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Search/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
