b67c00b1aefdc10380993af7f41858544b3a8b59
ngocphuphamm <ngocphupham682001@gmail.com>
2022-05-10 13:24:21 +0700

    adtocart

--- ClothesWebNET/Scripts/detail.js
+++ ClothesWebNET/Scripts/detail.js
@@ -30,15 +30,61 @@ const getSize = () => {
 let price = document.querySelector('.total-money').textContent;
 
 btnAddCart.addEventListener('click', (e) => {
-  let size = getSize();
-  if (size != null) {
-    let img = document.querySelector('#imagebox').src;
-    let totalMoney = document.querySelector('.total-money').textContent;
-    let title = document.querySelector('.detail-title').textContent;
-    let amount = document.querySelector('.input-amout').value;
-    let data = [{ title, price, totalMoney, size, img, amount }];
-    console.log(data);
-  } else console.log('bạn chưa chọn size');
+    let size = getSize();
+    if (size != null) {
+        let img = document.querySelector('#imagebox').src;
+        let totalMoney = document.querySelector('.total-money').textContent;
+        let title = document.querySelector('.detail-title').textContent;
+        let amount = document.querySelector('.input-amout').value;
+        let data = { title, price, totalMoney, size, img, amount };
+
+        var cart = window.localStorage.getItem('cart');
+        if (cart === null) {
+            window.localStorage.setItem('cart', JSON.stringify([data]));
+
+        }
+        else {
+            const listCart = JSON.parse(window.localStorage.getItem('cart'));
+          
+          
+             listCart.forEach((el) => {
+                if (el['title'] === data['title']) {
+                    if (el['size'] === data['size']) {
+                        const listCartAmount = Number(el['amount'])
+                        const dataAmount = Number(data['amount'])
+
+                        el['amount'] = listCartAmount + dataAmount;
+
+                    }
+                    else {
+           
+                    }
+                
+                }
+                else {
+                     const cartFake = listCart.find((el2) => {
+                              return el['title'] === el2['title']
+                          })
+                          if (!cartFake) {
+                              listCart.push(data)
+                          }
+                }
+            })
+           
+            let dataSize = listCart.find((el) => {
+                return el['title'] === data['title'] && el['size'] === data['size'] 
+            })
+            if (!dataSize) {
+               console.log("da vao")
+                listCart.push(data);
+            }
+            window.localStorage.setItem('cart', JSON.stringify(listCart));
+       
+
+      }
+
+    
+  } else alert('Bạn chưa chọn size');
 });
 
 //lấy giá trị từ localStorage với key=cart
