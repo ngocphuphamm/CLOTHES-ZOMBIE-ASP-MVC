a362b24a7ae5c2bf084ab688b985a9aa448e4cc5
Khang Nguyen <nguyenduykhanghuflit@gmail.com>
2022-05-25 10:43:10 +0700

    dashboard

--- ClothesWebNET/Areas/Admin/Controllers/DashboardController.cs
+++ ClothesWebNET/Areas/Admin/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClothesWebNET.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +9,21 @@ namespace ClothesWebNET.Areas.Admin.Controllers
 {
     public class DashboardController : Controller
     {
+        private CLOTHESEntities db = new CLOTHESEntities();
+
         // GET: Admin/Dashboard
         public ActionResult Index()
         {
-           
 
-           if (Session["SESSION_GROUP_ADMIN"] != null)
+
+            if (Session["SESSION_GROUP_ADMIN"] != null)
             {
+                int countUser = db.Users.Count();
+                int countBill = db.Bills.Count();
+                int countProduct = db.Products.Count();
+                ViewBag.countUser = countUser.ToString(); ;
+                ViewBag.countBill = countBill.ToString(); ;
+                ViewBag.product = countProduct.ToString(); ;
                 return View();
             }
             return Redirect("~/login");
